86749212f660a5fa1b8dd2b6f827c413350dd666
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-04-03 10:55:38 +0500

    Add calculation

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 12345;    //Цена билета
+        int rublePerMiles = 20;     //Кол-во рублей за 1 милю
+        int miles = priceTicket / rublePerMiles;    //Рассчет миль
 
+        System.out.println("Начислено миль: " + miles);
+
+
+    }
+}
